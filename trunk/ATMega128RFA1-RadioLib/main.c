5c600fc8cdd10f19cd36e02b88b8951ba1488ff0
ratmandu <ratmandu@10611cd7-829f-2697-c1f5-7c7a2a9c9395>
2012-01-28 23:19:53 +0000

    Added serial library

--- trunk/ATMega128RFA1-RadioLib/main.c
+++ trunk/ATMega128RFA1-RadioLib/main.c
@@ -6,36 +6,32 @@
  */
 
 #include <avr/interrupt.h>
-#include <avr/io.h>
 #include <util/delay.h>
 #include <avr/power.h>
+#include <avr/io.h>
 #include "radio.c"
 
+
 int main(void) {
-//	uint8_t resetReason = MCUSR;
-	MCUSR = 0;
 	clock_prescale_set(clock_div_1);
 
 	DDRG = (1<<PG0);
-//	if (resetReason & PORF) {
-//		PORTG = (1<<PG0);
-//		while (1)
-//			;
-//	}
+
 	cli();
 	trx_radioInit();
 	sei();
+
 	while (1) {
 		;
-//		sei();
-//		unsigned char data[2];
-//		data[0] = 'T';
-//		data[1] = 'E';
-//		trx_send(&data, sizeof(data));
-//		PORTG |= (1<<PG0);
-//		_delay_ms(500);
-//		PORTG &= ~(1<<PG0);
-//		_delay_ms(500);
+		sei();
+		unsigned char data[2];
+		data[0] = 'T';
+		data[1] = 'E';
+		trx_send(&data, sizeof(data));
+		PORTG |= (1<<PG0);
+		_delay_ms(500);
+		PORTG &= ~(1<<PG0);
+		_delay_ms(500);
 	}
 
 	return 1;
