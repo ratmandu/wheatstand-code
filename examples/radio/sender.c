accb28d4d3de97243d1f3f7ca4c5c218d8b57854
brimston3@gmail.com <brimston3@gmail.com@10611cd7-829f-2697-c1f5-7c7a2a9c9395>
2012-03-31 22:00:45 +0000

    * More code that works

--- examples/radio/sender.c
+++ examples/radio/sender.c
@@ -26,12 +26,16 @@ int main(void) {
 	while (1) {
 		// led on
 		PORTG |= (1<<PG0);
-		_delay_ms(500);
 		trx_send((void*)"hi", 2);
+		_delay_ms(500);
 		// led off
 		PORTG &= ~(1<<PG0);
 		_delay_ms(500);
 	}
 	// if we ever get here, something bad happened
 	return 1;
+}
+
+
+void receive_radio(uint8_t** frameData, uint8_t frameLength) {
 }
