db66f907ebd30616c1f5c0780a4f8d0af0d8739a
brimston3@gmail.com <brimston3@gmail.com@10611cd7-829f-2697-c1f5-7c7a2a9c9395>
2012-03-31 01:57:56 +0000

    * Adding simple blink example

---
+++ examples/blink/main.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 30, 2012
+ *      Author: brimstone
+ */
+
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/power.h>
+#include <util/atomic.h>
+#include <avr/io.h>
+
+int main(void) {
+	// need this for setup
+	clock_prescale_set(clock_div_1);
+
+	// setup our onboard led
+	DDRG = 0;
+	DDRG |= (1<<PG0);
+
+	while (1) {
+		// led on
+		PORTG |= (1<<PG0);
+		_delay_ms(500);
+		// led off
+		PORTG &= ~(1<<PG0);
+		_delay_ms(500);
+	}
+	// if we ever get here, something bad happened
+	return 1;
+}
