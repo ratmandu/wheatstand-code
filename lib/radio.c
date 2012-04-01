99981c6f8f6523bf8ae009e32aaab4f62c3c9999
brimston3@gmail.com <brimston3@gmail.com@10611cd7-829f-2697-c1f5-7c7a2a9c9395>
2012-04-01 02:17:15 +0000

    * Adding radio library

---
+++ lib/radio.c
@@ -0,0 +1,203 @@
+/*
+ * radio.c
+ *
+ *  Created on: Jan 15, 2012
+ *      Author: ratmandu
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <radio.h>
+
+void trx_radioInit() {
+//	uint8_t addr[8];
+//	addr[0] = MAC_ADDR_0;
+//	addr[1] = MAC_ADDR_1;
+//	addr[2] = MAC_ADDR_2;
+//	addr[3] = MAC_ADDR_3;
+//	addr[4] = MAC_ADDR_4;
+//	addr[5] = MAC_ADDR_5;
+//	addr[6] = MAC_ADDR_6;
+//	addr[7] = MAC_ADDR_7;
+//
+	IRQ_STATUS |= (1 << TX_END) | (1 << RX_END) | (1 << RX_START);
+	IRQ_MASK |= (1 << TX_END_EN) | (1 << RX_END_EN) | (1 << RX_START_EN);
+
+	trx_setTRXState(STATE_FORCE_TRX_OFF);
+	while (trx_getTRXState() != STATUS_TRX_OFF);
+//	trx_setExtAddr(addr);
+	trx_setChannel(RADIO_CHANNEL);
+	trx_setPANID(PAN_ADDR);
+	trx_setTRXState(STATE_RX_ON);
+	while (trx_getTRXState() != STATUS_RX_ON);
+
+//	trx_calibrate();
+
+}
+
+void trx_setChannel(uint8_t channel) {
+	PHY_CC_CCA = ((PHY_CC_CCA & ~0x1F) | (channel & 0x1F));
+}
+
+void trx_calibrate() {
+	FTN_CTRL |= (1 << FTN_START);
+    PLL_CF |= (1 << PLL_CF_START);
+    PLL_DCU |= (1 << PLL_DCU_START);
+}
+
+void trx_setTXPower(uint8_t power) {
+	PHY_TX_PWR = ((PHY_TX_PWR & ~0x0F) | (power & 0x0F));
+}
+
+void trx_setRXSensitivity(uint8_t level) {
+	RX_SYN = ((RX_SYN & ~0x0F) | (level & 0x0F));
+}
+
+void trx_send(void* data, uint8_t len) {
+	if (len > 125) {
+		return;
+	}
+
+	// make sure we arent receiving anything
+	trx_waitWhile(STATUS_BUSY_RX);
+
+	// turn on PLL and prepare for transmission
+	trx_setTRXState(STATE_PLL_ON);
+
+	// we are using automatic CRC, so we need to send 2 extra bytes
+	TRXFBST = len + 2;
+
+	// insert into transmission buffer
+	memcpy((void*)&TRXFBST+1, data, len);
+
+//	TRX_BUF(i+1) = 't';
+
+	while (trx_getTRXState() != STATUS_PLL_ON)
+		;
+
+	// start transmission
+	trx_setTRXState(STATE_TX_START);
+
+}
+
+void trx_setTRXState(uint8_t state) {
+	while ((TRX_STATUS & 0x1F) == 0x1F)
+		;
+	TRX_STATE = state;
+}
+
+void trx_enterTRXSleep() {
+	trx_setTRXState(STATE_TRX_OFF);
+}
+
+void trx_leaveTRXSleep() {
+	trx_setTRXState(STATE_RX_ON);
+}
+
+void trx_setPANID(uint16_t panid) {
+	PAN_ID_0 = (panid & 0xFF);
+	PAN_ID_1 = ((panid >> 8) &0xFF);
+}
+
+void trx_setShortAddress(uint16_t shortaddr) {
+	SHORT_ADDR_0 = (shortaddr & 0xFF);
+	SHORT_ADDR_1 = (shortaddr >>8) &0xFF;
+}
+
+void trx_setExtAddr(uint8_t* extaddr) {
+	IEEE_ADDR_0 = extaddr[0];
+	IEEE_ADDR_1 = extaddr[1];
+	IEEE_ADDR_2 = extaddr[2];
+	IEEE_ADDR_3 = extaddr[3];
+	IEEE_ADDR_4 = extaddr[4];
+	IEEE_ADDR_5 = extaddr[5];
+	IEEE_ADDR_6 = extaddr[6];
+	IEEE_ADDR_7 = extaddr[7];
+}
+
+void trx_waitWhile(uint8_t status) {
+	while (trx_getTRXState() == status) {
+		;
+	}
+}
+
+uint16_t trx_getShortAddress() {
+	uint16_t shortAddr;
+	shortAddr = (SHORT_ADDR_H << 8);
+	shortAddr |= SHORT_ADDR_L;
+	return shortAddr;
+}
+
+uint16_t trx_getPANID() {
+	uint16_t panID;
+	panID = (PAN_ID_1 << 8);
+	panID |= PAN_ID_0;
+	return panID;
+}
+
+void trx_getExtAddr(uint8_t* addr) {
+	addr[0] = IEEE_ADDR_0;
+	addr[1] = IEEE_ADDR_1;
+	addr[2] = IEEE_ADDR_2;
+	addr[3] = IEEE_ADDR_3;
+	addr[4] = IEEE_ADDR_4;
+	addr[5] = IEEE_ADDR_5;
+	addr[6] = IEEE_ADDR_6;
+	addr[7] = IEEE_ADDR_7;
+}
+
+uint8_t trx_isSleeping() {
+	if (trx_getTRXState() == STATUS_SLEEP)
+		return 1;
+	else
+		return 0;
+}
+
+uint8_t trx_getTRXState() {
+	return (TRX_STATUS & TRX_STATUS_MASK);
+}
+
+uint8_t trx_getRandomNumber() {
+	uint8_t rand;
+	rand = ((PHY_RSSI & PHY_RSSI_RANDOM_MASK) >> 5);
+	_delay_us(2);
+	rand |= ((PHY_RSSI & PHY_RSSI_RANDOM_MASK) >> 3);
+	_delay_us(2);
+	rand |= ((PHY_RSSI & PHY_RSSI_RANDOM_MASK) >> 1);
+	_delay_us(2);
+	rand |= ((PHY_RSSI & PHY_RSSI_RANDOM_MASK) << 1);
+	return rand;
+}
+
+uint8_t trx_getRSSI() {
+	return PHY_RSSI & PHY_RSSI_MASK;
+}
+
+ISR(TRX24_TX_END_vect) {
+	trx_setTRXState(STATE_RX_ON);
+}
+
+ISR(TRX24_RX_END_vect) {
+	if (ReadBit(PHY_RSSI, RX_CRC_VALID) == 0) {
+		return;
+	}
+	uint8_t frameData[125];
+	uint8_t frameLength = 0;
+
+	uint8_t length = TST_RX_LENGTH;
+
+	length -= 2;
+
+	frameLength = length;
+
+	memcpy(frameData, (void*)&TRXFBST, (uint8_t)TRXFBST);
+	frameData[frameLength]=0;
+	// TODO see if this is actually defined
+	receive_radio(frameData, frameLength);
+}
+
+ISR(TRX24_RX_START_vect) {
+	rssi = PHY_RSSI;
+}
---
+++ lib/radio.h
@@ -0,0 +1,108 @@
+/*
+ * radio.h
+ *
+ *  Created on: Jan 15, 2012
+ *      Author: ratmandu
+ */
+
+#ifndef RADIO_H_
+#define RADIO_H_
+
+#define TRX_MAX_FRAME_SIZE 127
+#define TRX_FIFO_SIZE 128
+#define TRX_WAIT_BEFORE
+//#define TRX_WAIT_AFTER
+
+#define SHORT_ADDR_L 0xFE
+#define SHORT_ADDR_H 0xED
+
+#define RADIO_CHANNEL 20
+
+#define PAN_ADDR 0xACDC
+
+#define MAC_ADDR_0 0x01
+#define MAC_ADDR_1 0x23
+#define MAC_ADDR_2 0x45
+#define MAC_ADDR_3 0x67
+#define MAC_ADDR_4 0x89
+#define MAC_ADDR_5 0xab
+#define MAC_ADDR_6 0xcd
+#define MAC_ADDR_7 0xef
+
+#define modifyReg(var, mask, value)   (var = ((var & ~mask) | (value & mask)))
+
+// Transceiver state status
+#define TRX_STATUS_MASK    0x1f
+enum
+{
+        STATUS_BUSY_RX = 0x01,
+        STATUS_BUSY_TX = 0x02,
+        STATUS_RX_ON = 0x06,
+        STATUS_TRX_OFF = 0x08,
+        STATUS_PLL_ON = 0x09,
+        STATUS_SLEEP = 0x0f,
+        STATUS_BUSY_RX_AACK = 0x11,
+        STATUS_BUSY_TX_ARET = 0x12,
+        STATUS_RX_AACK_ON = 0x16,
+        STATUS_TX_ARET_ON = 0x19,
+        STATUS_STATE_TRANSITION_IN_PROGRESS = 0x1f
+};
+
+// Transceiver state change commands
+enum
+{
+        STATE_NOP = 0x00,
+        STATE_TX_START = 0x02,
+        STATE_FORCE_TRX_OFF = 0x03,
+        STATE_FORCE_PLL_ON = 0x04,
+        STATE_RX_ON = 0x06,
+        STATE_TRX_OFF = 0x08,
+        STATE_PLL_ON = 0x09,
+        STATE_RX_AACK_ON = 0x16,
+        STATE_TX_ARET_ON = 0x19
+};
+
+#define PHY_RSSI_MASK 			0x1f
+#define PHY_RSSI_RANDOM_MASK	0x60
+#define TRX_BUF(i) 				_SFR_MEM8(0x180 + i)
+#define ReadBit(var, bit) (((var) & (1 << (bit))) != 0)
+
+static volatile uint8_t rssi;
+
+enum
+{
+        VERSION_A = 2, // A and B are both 2 according to the data sheet
+        VERSION_B = 2,
+        VERSION_C = 3,
+        VERSION_D = 4
+};
+
+uint8_t currentlyTransmitting;
+
+void trx_radioInit();
+void trx_setChannel(uint8_t channel);
+void trx_send(void* data, uint8_t len);
+void trx_calibrate();
+void trx_setTXPower(uint8_t power);
+void trx_setRXSensitivity(uint8_t level);
+void trx_setTRXState(uint8_t state);
+void trx_enterTRXSleep();
+void trx_leaveTRXSleep();
+void trx_setPANID(uint16_t panid);
+void trx_setShortAddress(uint16_t shortaddr);
+void trx_setExtAddress(uint8_t* extaddr);
+void trx_waitWhile(uint8_t status);
+void trx_getExtAddress(uint8_t* addr);
+
+void receive_radio(uint8_t* frameData, uint8_t frameLength);
+
+uint16_t trx_getShortAddress();
+uint16_t trx_getPANID();
+uint8_t trx_isSleeping();
+uint8_t trx_getTRXState();
+uint8_t trx_getRandomNumber();
+uint8_t trx_getRSSI();
+
+
+
+#endif /* RADIO_H_ */
